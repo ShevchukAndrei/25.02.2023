3d80cc81ab14f04a08e25fe32b64d336334d0479
Andrei <yhn@mail.ru>
2023-02-26 11:03:14 +0300

    семинарий

---
+++ Task10/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число из отрезка 100 - 1000 -> {number}");
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
+// int result =  firstDigit*10 + lastDigit;
+// Console.WriteLine($"{result}");
+
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число -> {number}");
+
+
+int removeSecondDigit = RemoveSecondDigit(number);
+Console.WriteLine($"Результат = {removeSecondDigit}");
+
+int RemoveSecondDigit (int num) 
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+}
