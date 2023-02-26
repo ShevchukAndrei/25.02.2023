31092cd38e4ef74ff9dca0f2dc68c713ed4d13ac
Andrei <yhn@mail.ru>
2023-02-26 13:01:56 +0300

    семинар

---
+++ Task14/Program.cs
@@ -0,0 +1,31 @@
+﻿// Console.WriteLine("введите число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// if (number1 % 7 ==0 && number1 % 23 == 0) {
+//     Console.WriteLine("да");
+// }
+// else { Console.WriteLine("нет");
+// }
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 7 == 0 && number % 23 == 0)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+bool IsDivided(int num)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+if (IsDivided(number))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
