1532624844563c7fde2d461de4265ce20b37f833
Andrei <yhn@mail.ru>
2023-03-04 20:58:50 +0300

    home work2

---
+++ Task15/Program.cs
@@ -0,0 +1,36 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+
+}
+bool ValidateWeekDay(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+int weekDay = Prompt("введите день недели: ");
+if (ValidateWeekDay(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("выходной");
+    }
+    else {
+         Console.WriteLine("будний день");
+    }
+    }
