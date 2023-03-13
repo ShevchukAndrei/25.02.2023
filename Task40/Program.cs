edce9ffef6d36ed585368b883d61a17da9d959f6
Andrei <yhn@mail.ru>
2023-03-13 20:30:23 +0300

    семинар 6

---
+++ Task40/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+
+
+Console.WriteLine("введите длинну отрезка  AB: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите  длинну отрезка BC: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите  длинну отрезка CA: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+bool isExistTriangle = IsExistTriangle(numberA, numberB, numberC);
+Console.WriteLine(isExistTriangle
+? "может существовать"
+: "не может существовать ");
+
+bool IsExistTriangle(int numA, int numB, int numC)
+{
+    return numA < numB + numC && numB < numA + numC && numC < numA + numB;
+}
+
+
---
+++ Task42/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("введите десятичное  число : ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(DecToBin( numberA)) ;
+
+int DecToBin(int num)
+{
+    int res =0;
+    int multiplier =1;
+    while(num>0)
+    {
+        res +=(num%2)*multiplier;
+        num=num/2;
+        multiplier *=10;
+    }
+    return res;
+}
+
+
+
