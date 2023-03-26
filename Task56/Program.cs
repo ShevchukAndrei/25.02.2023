7b953cf62337e80cc5cde38a5624739b3f8ca10b
Andrei <yhn@mail.ru>
2023-03-26 16:10:45 +0300

    home work

---
+++ Task56/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+//  которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+int[] MatrixToArray(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    int count = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        array[count] = sum;
+        count++;
+    }
+    return array;
+}
+
+void ArrayPrinted(int[] arr)
+{
+    Console.Write("сумма по строкам[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int SmallestSumElements(int[] array)
+{
+    int min = 0;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            min = i + 1;
+        }
+    }
+    return min+1;
+}
+
+
+int[,] matrixRndInt = CreateMatrixRndInt(5, 5, -1, 6);
+PrintMatrix(matrixRndInt);
+int[] matrixToArray = MatrixToArray(matrixRndInt);
+Console.WriteLine();
+ArrayPrinted(matrixToArray);
+int smallestSumElements = SmallestSumElements(matrixToArray);
+Console.WriteLine(($"строка с наименьшей суммой элементов №{smallestSumElements}"));
---
+++ Task58/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] ProductTwoMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] newMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            newMatrix[i, j] = 0;
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                newMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+
+        }
+    }
+    return newMatrix;
+}
+
+int[,] matrixRndInt1 = CreateMatrixRndInt(2, 3, 1, 4);
+PrintMatrix(matrixRndInt1);
+Console.WriteLine();
+int[,] matrixRndInt2 = CreateMatrixRndInt(3, 4, 1, 4);
+PrintMatrix(matrixRndInt2);
+if (matrixRndInt1.GetLength(1) != matrixRndInt2.GetLength(0))
+{
+    Console.WriteLine("количество колонн матрицы 1 не соответствует количеству строк матрицы 2");
+}
+else
+{
+    Console.WriteLine();
+    int[,] productTwoMatrix = ProductTwoMatrix(matrixRndInt1, matrixRndInt2);
+    PrintMatrix(productTwoMatrix);
+}
